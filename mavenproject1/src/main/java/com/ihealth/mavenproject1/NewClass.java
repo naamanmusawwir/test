5f98fe8125e4634575860f1d21b5f9cc15dfeeb2
Naaman Musawwir <naaman@truemeridian.com>
2015-05-14 18:27:08 +0500

    Test Commit

---
+++ mavenproject1/src/main/java/com/ihealth/mavenproject1/NewClass.java
@@ -0,0 +1,12 @@
+package com.ihealth.mavenproject1;
+
+/**
+ * @author Naaman Musawwir
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        System.out.println("Test GitHub...");
+    }
+    
+}
